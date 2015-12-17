feb30ac1437a51f41f240e4c9072e9271f93caea
cjmabry <cjmab28@gmail.com>
2015-12-17 06:56:09 -0600

    Updated config for new RDS

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 PROPAGATE_EXCEPTIONS = False
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://reddimatch:cjm4441993@reddimatch-staging.ckuiwsflfftp.us-west-2.rds.amazonaws.com:3306/db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://reddimatch:cjm4441993@rreddimatch.ckuiwsflfftp.us-west-2.rds.amazonaws.com:3306/main'
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 DATABASE_CONNECT_OPTIONS = {}
@@ -11,7 +11,7 @@ DATABASE_CONNECT_OPTIONS = {}
 REDDIT_USER_AGENT = 'web:com.reddimatch:v1.0.0 (by /u/cjmabry)'
 REDDIT_CLIENT_ID = 'R6NOkcOGXv1xUw'
 REDDIT_CLIENT_SECRET = '73pZJK5-lOxf5jRiW6YqJ-IMPgg'
-REDDIT_REDIRECT_URI = 'http://reddimatch-staging/authorize_callback'
+REDDIT_REDIRECT_URI = 'http://ec2-52-25-43-253.us-west-2.compute.amazonaws.com//authorize_callback'
 
 CSRF_ENABLED = True
 CSRF_SESSION_KEY = os.urandom(24)
