ec395c52fb4acb7f89b746465345976a07e920c7
cjmabry <cjmab28@gmail.com>
2015-12-17 18:55:08 -0600

    Modified production config

--- config.py
+++ config.py
@@ -3,12 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 PROPAGATE_EXCEPTIONS = False
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://reddimatch:cjm4441993@reddimatch.ckuiwsflfftp.us-west-2.rds.amazonaws.com:3306/main'
-
-# if os.environ.get('DATABASE_URL') is None:
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-# else:
-#     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://reddimatch:reddimatchpassword123@reddimatch.cbbaiqdon3aq.us-west-2.rds.amazonaws.com:3306/main'
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 DATABASE_CONNECT_OPTIONS = {}
--- config_prod.py
+++ config_prod.py
@@ -3,15 +3,15 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 PROPAGATE_EXCEPTIONS = False
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://reddimatch:cjm4441993@reddimatch.ckuiwsflfftp.us-west-2.rds.amazonaws.com:3306/main'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://reddimatch:reddimatchpassword123@reddimatch.cbbaiqdon3aq.us-west-2.rds.amazonaws.com:3306/main'
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 DATABASE_CONNECT_OPTIONS = {}
 
 REDDIT_USER_AGENT = 'web:com.reddimatch:v1.0.0 (by /u/cjmabry)'
-REDDIT_CLIENT_ID = 'R6NOkcOGXv1xUw'
-REDDIT_CLIENT_SECRET = '73pZJK5-lOxf5jRiW6YqJ-IMPgg'
-REDDIT_REDIRECT_URI = 'http://ec2-54-200-155-165.us-west-2.compute.amazonaws.com/authorize_callback'
+REDDIT_CLIENT_ID = 'DvvmmJvVVRVShA'
+REDDIT_CLIENT_SECRET = '9Rjy8YfqMLK0cy_edPRNL82zTm4'
+REDDIT_REDIRECT_URI = 'http://reddimatch.com/authorize_callback'
 
 CSRF_ENABLED = True
 CSRF_SESSION_KEY = os.urandom(24)
