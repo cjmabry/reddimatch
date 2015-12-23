77030df902c9f24f351ae0e0e15964ee4b6ad583
cjmabry <cjmab28@gmail.com>
2015-12-23 00:25:58 -0600

    Fixed staging config

--- config/config_stage.py
+++ config/config_stage.py
@@ -11,7 +11,7 @@ DATABASE_CONNECT_OPTIONS = {}
 REDDIT_USER_AGENT = 'web:com.reddimatch:v1.0.0 (by /u/cjmabry)'
 REDDIT_CLIENT_ID = 'R6NOkcOGXv1xUw'
 REDDIT_CLIENT_SECRET = '73pZJK5-lOxf5jRiW6YqJ-IMPgg'
-REDDIT_REDIRECT_URI = 'http://http://ec2-52-33-249-208.us-west-2.compute.amazonaws.com/authorize_callback'
+REDDIT_REDIRECT_URI = 'http://ec2-52-33-249-208.us-west-2.compute.amazonaws.com/authorize_callback'
 
 CSRF_ENABLED = True
 CSRF_SESSION_KEY = os.urandom(24)
