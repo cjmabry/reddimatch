4127e7ba05b2f0e0aa09e3192f077fec5907c295
cjmabry <cjmab28@gmail.com>
2016-01-18 21:41:31 -0600

    Tweaked oauth generator

--- generate_oauth_config.py
+++ generate_oauth_config.py
@@ -1,8 +1,8 @@
 import ConfigParser, os
 config = ConfigParser.ConfigParser()
 config.read('./oauth_template.ini')
-config.set('app','app_key',os.environ['REDDIT_NOTIFIER_ID'])
-config.set('app','app_secret',os.environ['REDDIT_NOTIFIER_SECRET'])
+config.set('app','app_key',os.environ.get('REDDIT_NOTIFIER_ID'))
+config.set('app','app_secret',os.environ.get('REDDIT_NOTIFIER_SECRET'))
 
 with open(r'./oauth_config.ini', 'wb') as configfile:
     config.write(configfile)
