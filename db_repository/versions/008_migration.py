93026e63242d970106e028a2b32da4e5e05773fc
cjmabry <cjmab28@gmail.com>
2016-01-28 19:30:22 -0600

    Added Gulp, WIP

---
+++ db_repository/versions/008_migration.py
@@ -0,0 +1,54 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+user = Table('user', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('username', String(length=80)),
+    Column('reddit_username', String(length=80)),
+    Column('email', String(length=255)),
+    Column('refresh_token', String(length=60)),
+    Column('age', Integer),
+    Column('gender_id', Integer),
+    Column('desired_gender_id', Integer),
+    Column('orientation', String(length=60)),
+    Column('location', String(length=120)),
+    Column('postal_code', String(length=10)),
+    Column('latitude', Float(precision=10)),
+    Column('longitude', Float(precision=10)),
+    Column('bio', String(length=140)),
+    Column('profile_photo_url', String(length=120)),
+    Column('registered', Boolean),
+    Column('email_verified', Boolean),
+    Column('created_on', DateTime),
+    Column('verified_on', DateTime),
+    Column('last_online', DateTime),
+    Column('is_online', Boolean),
+    Column('newsletter', Boolean),
+    Column('date_searchable', Boolean),
+    Column('min_age', Integer),
+    Column('max_age', Integer),
+    Column('search_radius', Integer),
+    Column('oauth_denied', Boolean),
+    Column('allow_reddit_notifications', Boolean),
+    Column('deleted', Boolean),
+    Column('disable_location', Boolean),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['user'].columns['disable_location'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['user'].columns['disable_location'].drop()
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    minifyCss = require('gulp-minify-css'),
+    useref = require('gulp-useref'),
+    gulpIf = require('gulp-if');
+
+var exec = require('child_process').exec;
+
+// Run python server
+gulp.task('runserver', function() {
+  var proc = exec('python run.py');
+});
+
+// browserSync
+gulp.task('browserSync', function() {
+  browserSync.init({
+    notify: false,
+    proxy: "127.0.0.1:8000"
+  });
+});
+
+// Watchers
+gulp.task('watch', function() {
+  // gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/static/js/**/*.js', browserSync.reload);
+});
+
+// Optimization Tasks
+
+// JS
+gulp.task('uglify', function() {
+  return gulp.src('app/static/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/static/js'));
+});
+
+// CSS
+gulp.task('minify-css', function() {
+  return gulp.src('app/static/css/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/static/css'));
+});
+
+// Images
+gulp.task('imagemin', function(){
+  return gulp.src('app/static/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest('dist/static/images'));
+});
+
+// Copy HTML
+gulp.task('html', function() {
+  return gulp.src('app/templates/*.html')
+  .pipe(gulp.dest('dist/templates'));
+});
+
+// Copy Python
+gulp.task('python', function() {
+  return gulp.src('app/*.py')
+  .pipe(gulp.dest('dist'));
+});
+
+// Copy Fonts
+gulp.task('fonts', function() {
+  return gulp.src('app/static/fonts/**/*')
+  .pipe(gulp.dest('dist/static/fonts'));
+});
+
+// Cleaning
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+
+// Build Sequences
+gulp.task('default', function(callback) {
+  runSequence(['runserver', 'browserSync', 'watch'],
+    callback
+  );
+});
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist',
+    [ 'python','html','minify-css','uglify','imagemin', 'fonts'],
+    callback
+  );
+});
