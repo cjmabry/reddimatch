85a0a36d635f97de904179cf3cbcae0ddc6b8e3d
cjmabry <cjmab28@gmail.com>
2016-02-15 19:21:28 -0600

    Mysql fix

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get -y update
-sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev git monit python-pip nginx
+sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev git libmysqlclient-dev mysql monit python-pip nginx
