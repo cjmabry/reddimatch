64a9685872598ce4ada2cf658388aa8203631e40
cjmabry <cjmab28@gmail.com>
2016-02-16 19:39:52 -0600

    Moved env stuff to user data on AWS

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
-export AWS_DEFAULT_REGION=us-west-2
-
-get_ami_tags () {
-    instance_id=$(curl --silent http://169.254.169.254/latest/meta-data/instance-id)
-    echo $(aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id")
-}
-
-get_instance_tags () {
-    ami_id=$(curl --silent http://169.254.169.254/latest/meta-data/ami-id)
-    echo $(aws ec2 describe-tags --filters "Name=resource-id,Values=$ami_id")
-}
-
-tags_to_env () {
-    tags=$1
-
-    for key in $(echo $tags | jq -r ".[][].Key"); do
-        value=$(echo $tags | jq -r ".[][] | select(.Key==\"$key\") | .Value")
-        key=$(echo $key | tr '-' '_' | /usr/bin/tr '[:lower:]' '[:upper:]')
-        export $key="$value"
-    done
-}
-
-ami_tags=$(get_ami_tags)
-instance_tags=$(get_instance_tags)
-
-tags_to_env "$ami_tags"
-tags_to_env "$instance_tags"
-
 sudo apt-get -y update
 sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev git libmysqlclient-dev monit python-pip nginx jq
